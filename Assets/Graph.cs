8871004d0409c085e6128ea7c2a73cc68ba6d821
bjornaah <bjornaah@stud.ntnu.no>
2019-11-20 23:01:51 +0100

    Added richards stuff

---
+++ Assets/Graph.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Graph : MonoBehaviour
+{
+    [System.Serializable]
+    public class Edge
+    {
+        public GraphNode StartNode;
+
+        public GraphNode EndNode;
+
+        [Range(0f, 10f)]
+        public int Weight;
+    }
+
+    public List<GraphNode> Nodes;
+    public List<Edge> Edges; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Reset()
+    {
+        Nodes = GetComponentsInChildren<GraphNode>(true).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
