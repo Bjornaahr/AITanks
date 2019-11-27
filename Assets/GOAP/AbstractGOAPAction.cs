0041d308958792f59b8d551ba6210eb2bb181d6a
bjornaah <bjornaah@stud.ntnu.no>
2019-11-27 08:08:38 +0100

    Started on GOAP

---
+++ Assets/GOAP/AbstractGOAPAction.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractGOAPAction : MonoBehaviour
+{
+    private HashSet<KeyValuePair<string, object>> preconditions;
+    private HashSet<KeyValuePair<string, object>> effects;
+
+    private bool inRange = false;
+
+
+    public float cost = 1f;
+
+    public GameObject target;
+
+    public AbstractGOAPAction()
+    {
+        preconditions = new HashSet<KeyValuePair<string, object>>();
+        effects = new HashSet<KeyValuePair<string, object>>();
+    }
+
+    public void doReset()
+    {
+        inRange = false;
+        target = null;
+        reset();
+    }
+    //Resets variables needed before planning again
+    public abstract void reset();
+
+    public abstract bool isDone();
+
+    //Checks if the action can be performed based on precondtions
+    public abstract bool checkPrecondtion(GameObject agent);
+
+    public abstract bool perform(GameObject agent);
+
+    public abstract bool requiresInRange();
+
+    public bool isInRange()
+    {
+        return inRange;
+    }
+
+    public void setInRange(bool inRange)
+    {
+        this.inRange = inRange;
+    }
+
+    public void addPrecondition(string key, object value)
+    {
+        preconditions.Add(new KeyValuePair<string, object>(key, value));
+    }
+
+
+    public void removePrecondition(string key)
+    {
+        KeyValuePair<string, object> remove = default(KeyValuePair<string, object>);
+        foreach (KeyValuePair<string, object> kvp in preconditions)
+        {
+            if (kvp.Key.Equals(key))
+                remove = kvp;
+        }
+        if (!default(KeyValuePair<string, object>).Equals(remove))
+            preconditions.Remove(remove);
+    }
+
+
+    public void addEffect(string key, object value)
+    {
+        effects.Add(new KeyValuePair<string, object>(key, value));
+    }
+
+
+    public void removeEffect(string key)
+    {
+        KeyValuePair<string, object> remove = default(KeyValuePair<string, object>);
+        foreach (KeyValuePair<string, object> kvp in effects)
+        {
+            if (kvp.Key.Equals(key))
+                remove = kvp;
+        }
+        if (!default(KeyValuePair<string, object>).Equals(remove))
+            effects.Remove(remove);
+    }
+
+
+    public HashSet<KeyValuePair<string, object>> Preconditions
+    {
+        get
+        {
+            return preconditions;
+        }
+    }
+
+    public HashSet<KeyValuePair<string, object>> Effects
+    {
+        get
+        {
+            return effects;
+        }
+    }
+
+
+}
