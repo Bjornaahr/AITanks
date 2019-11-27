43fad4aaba04d842c7b4d0461e131f86792b28d9
ScarredSceptile <jonas.haegd@gmail.com>
2019-11-27 22:49:27 +0100

    Added a graph that spawns in by code. Was a pain to code

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -16,13 +16,148 @@ public class Graph : MonoBehaviour
         public int Weight;
     }
 
+    [SerializeField]
+    private GameObject floor;
+    [SerializeField]
+    private GameObject node;
+
     public List<GraphNode> Nodes;
     public List<Edge> Edges; 
 
     // Start is called before the first frame update
     void Start()
     {
+
+        Vector3[] floorVerticies = floor.GetComponent<MeshFilter>().sharedMesh.vertices;
+
+        int iL = ((int)floorVerticies[0].x * (int)floor.transform.localScale.x - (int)floorVerticies[120].x * (int)floor.transform.localScale.x) / 3 + 1;
+        int jL = ((int)floorVerticies[0].z * (int)floor.transform.localScale.z - (int)floorVerticies[120].z * (int)floor.transform.localScale.z) / 3 + 1;
         
+
+        int[,] nodeList = new int[iL, jL];
+
+        int i = 0, j = 0;
+
+        int ID = 0;
+        
+        for (float x = (int)floorVerticies[120].x * (int)floor.transform.localScale.x; x < (int)floorVerticies[0].x * (int)floor.transform.localScale.x; x += 3)
+        {
+            j = 0;
+            for (float y = (int)floorVerticies[120].z * (int)floor.transform.localScale.z; y < (int)floorVerticies[0].z * (int)floor.transform.localScale.z; y += 3)
+            {
+                GameObject newNode = Instantiate(node) as GameObject;
+                newNode.name = ID.ToString();
+                newNode.transform.parent = gameObject.transform;
+                newNode.transform.position = new Vector3(x, 1, y);
+
+                if (!Physics.CheckSphere(newNode.transform.position, 0.9f))
+                {
+                    newNode.transform.position = new Vector3(x, 0, y);
+                    Nodes.Add(newNode.GetComponent<GraphNode>());
+
+                    nodeList[i, j] = ID;
+                    ID++;
+                }
+                else
+                {
+                    nodeList[i, j] = -1;
+                    Destroy(newNode);
+                }
+                j++;
+            }
+            i++;
+        }
+        print(i + " " + iL);
+        print(j + " " + jL);
+        for (i = 0; i < iL; i++)
+        {
+            for (j = 0; j < jL; j++)
+            {
+
+                ID = nodeList[i, j];
+                //Is there a node in this spot?
+                if (ID != -1)
+                {
+                    if (i == 0)
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i + 1, j];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+
+                    else if (i == iL - 1)
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i - 1, j];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+
+                    else
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i + 1, j];
+                        int adjNode2 = nodeList[i - 1, j];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                        if (adjNode2 != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode2];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+
+                    if (j == 0)
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i, j + 1];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+
+                    else if (j == jL - 1)
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i, j - 1];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+
+                    else
+                    {
+                        GraphNode thisNode = Nodes[ID];
+                        int adjNode = nodeList[i, j + 1];
+                        int adjNode2 = nodeList[i, j - 1];
+                        if (adjNode != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                        if (adjNode2 != -1)
+                        {
+                            GraphNode adjacentNode = Nodes[adjNode2];
+                            thisNode.Adjacent.Add(adjacentNode);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private void Reset()
