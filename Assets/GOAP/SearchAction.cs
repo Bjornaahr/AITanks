128d44f7e5814a100aede7b3029307e9ae535b15
bjornaah <bjornaah@stud.ntnu.no>
2019-11-28 16:24:54 +0100

    Basics of GOAP in place

---
+++ Assets/GOAP/SearchAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchAction : AbstractGOAPAction
+{
+
+    public SearchAction()
+    {
+        addPrecondition("canSeeEnemy", false);
+        addPrecondition("hasEnoughHealth", true);
+    }
+
+
+    //Check if we have last known position, if not take it random walk
+    public override bool checkPrecondtion(GameObject agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool isDone()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //Walk towards node, choose node until enemy is seen
+    public override bool perform(GameObject agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool requiresInRange()
+    {
+        return false;
+    }
+
+    public override void reset()
+    {
+        
+    }
+
+}
